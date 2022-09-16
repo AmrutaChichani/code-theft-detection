705e4ebd7aeb9b88bfb9b6d932ee0eb4d588a983
Amruta Chichani <amruta.chichani@clairvoyantsoft.com>
2022-09-16 16:15:44 +0530

    Demo for detecting code theft

---
+++ Demo/InputSanitizationFilter.java
@@ -0,0 +1,9 @@
+package Demo;
+
+public class InputSanitizationFilter {
+    //DEMO FILE - FOR CODE THEFT DETECTION
+    String url = "https://artifact.internal.shutterfly.com";
+     public void appCode(){
+        System.out.println("Attempting to configure prevoty for Appcode");
+    }
+}
